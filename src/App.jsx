b5d8f623dd4922b45ba76252314edf82f5ee5bb3
Nadiia-Yuce <soyka9616@gmail.com>
2024-08-15 11:35:22 +0200

    Feedback widget

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,69 @@
-// ----- LocalSrtorage pattern: -----
-  
-//   import { useState, useEffect } from 'react';
-  
-// **Функція, яка витягує і повертає значення з локал стореджу ЯКЩО ВОНО ПРИСУТНЄ, а якщо його не має, то повертає якесь дефолтне значення.Це значення буде початковим для стану компонента.
+import Description from "./components/Description/Description";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
+import Notification from "./components/Notification/Notification";
+import { useState, useEffect } from "react";
 
-// function getInitialValues() {
-//   const savedValues = window.localStorage.getItem("savedValues");
-//   return savedValues !== null
-//     ? JSON.parse(savedValues)
-//     : {
-//         good: 0,
-//         neutral: 0,
-//         bad: 0,
-//       };
-// }
+//Функція для отримання значеня з локал стореджу (або надавання дефолтного) як початкового для стану компоненту
+function getInitialOptions() {
+  const savedOptions = window.localStorage.getItem("savedOptions");
+  return savedOptions !== null
+    ? JSON.parse(savedOptions)
+    : {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      };
+}
 
-// **Запис компонента Арр
+export default function App() {
+  //Оголошення стану, який буде локально фіксувати зміни з початковим значенням - результатом фиконання функції
+  const [options, setOptions] = useState(getInitialOptions);
 
-// export const App () => {
+  //Записуємо актуальний стан компонента після оновлення в локал сторедж
+  useEffect(() => {
+    window.localStorage.setItem("savedOptions", JSON.stringify(options));
+  }, [options]);
 
-// ** Задаємо стан на компонент і даємо йому початковим значення результат виконання ф - ї з отримання початково значення
-  
-// const [values, setValues] = useState(getInitialValues);
+  //Підрахунок загальної кількості фідбеків
+  const totalFeedback = options.good + options.neutral + options.bad;
 
-//   ** Задаємо ефект на стан компонента, у якому записуємо нове значення(після оновлення компоненту) в локал сторедж.
-    
-//   useEffect(() => {
-//     window.localStorage.setItem("savedValues", JSON.stringify(values));
-//   }, [values]);
+  //При зміні властивості об'єкту або масиву преба обов'язково робити копію (через ...) для уникнення мутації і втрати значень
+  function updateFeedback(feedbackType) {
+    setOptions({ ...options, [feedbackType]: options[feedbackType] + 1 });
+  }
 
-//   ** Повертаємо якусь розмітку
-  
-//   return (<div> <Component someProp="propValue" /></div>);
-// }
-// _______________________________________________________________________________________________________________________________
+  //Функція скидання значень фідбеку
+  function resetFeedback() {
+    setOptions({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  }
+
+  return (
+    <div>
+      <Description />
+      <Options update={updateFeedback} type="good">
+        Good
+      </Options>
+      <Options update={updateFeedback} type="neutral">
+        Neutral
+      </Options>
+      <Options update={updateFeedback} type="bad">
+        Bad
+      </Options>
+      {totalFeedback > 0 && (
+        <Options reset={resetFeedback} action="reset">
+          Reset
+        </Options>
+      )}
+      {totalFeedback ? (
+        <Feedback options={options} total={totalFeedback} />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
---
+++ src/components/Tipps.jsx
@@ -0,0 +1,36 @@
+// ----- LocalSrtorage pattern: -----
+
+//   import { useState, useEffect } from 'react';
+
+// **Функція, яка витягує і повертає значення з локал стореджу ЯКЩО ВОНО ПРИСУТНЄ, а якщо його не має, то повертає якесь дефолтне значення.Це значення буде початковим для стану компонента.
+
+// function getInitialValues() {
+//   const savedValues = window.localStorage.getItem("savedValues");
+//   return savedValues !== null
+//     ? JSON.parse(savedValues)
+//     : {
+//         good: 0,
+//         neutral: 0,
+//         bad: 0,
+//       };
+// }
+
+// **Запис компонента Арр
+
+// export const App () => {
+
+// ** Задаємо стан на компонент і даємо йому початковим значення результат виконання ф - ї з отримання початково значення
+
+// const [values, setValues] = useState(getInitialValues);
+
+//   ** Задаємо ефект на стан компонента, у якому записуємо нове значення(після оновлення компоненту) в локал сторедж.
+
+//   useEffect(() => {
+//     window.localStorage.setItem("savedValues", JSON.stringify(values));
+//   }, [values]);
+
+//   ** Повертаємо якусь розмітку
+
+//   return (<div> <Component someProp="propValue" /></div>);
+// }
+// _______________________________________________________________________________________________________________________________
